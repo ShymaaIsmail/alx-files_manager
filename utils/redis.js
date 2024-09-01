77228fca3f29c1e7da981c18629a91768241b6eb
shymaaismail <shymaa.m.ismail@gmail.com>
2024-09-01 11:06:44 +0300

    "Updated Redis client implementation: added promisify for Redis methods, changed error handling, and updated Redis connection status checks."

--- utils/redis.js
+++ utils/redis.js
@@ -1,36 +1,42 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.isConnected = false;
     this.client = createClient();
     this.client.on('error', (err) => {
-      console.log(err);
-      this.client.quit();
+      console.error('Redis error:', err);
+    });
+
+    // Promisify Redis methods
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+
+    this.client.on('ready', () => {
+    });
+
+    this.client.on('end', () => {
     });
   }
 
   isAlive() {
-    return this.client.connected;
+    return this.client.ready;
   }
 
   async get(key) {
-    const value = await this.client.get(key);
+    const value = await this.getAsync(key);
     return value;
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value,
-      {
-        EX: duration,
-      });
+    await this.setexAsync(key, duration, value);
   }
 
   async del(key) {
-    const value = await this.client.del(key);
-    return value;
+    await this.delAsync(key);
   }
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
