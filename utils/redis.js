8d7af0495b3263afea422b38fb1a83b8089ba0ad
shymaaismail <shymaa.m.ismail@gmail.com>
2024-09-01 10:12:03 +0300

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(err));
+  }
+
+  isAlive() {
+    return this.client.isReady;
+  }
+
+  async get(key) {
+    const value = await this.client.get(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value,
+      {
+        EX: duration,
+      });
+  }
+
+  async del(key) {
+    const value = await this.client.del(key);
+    return value;
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
